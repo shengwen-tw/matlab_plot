c583b436dc6db0a7fad7a02c7b197f9452be2354
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-07-08 19:30:15 +0800

    add new gps accuracy data log

--- gps_accuracy/main.m
+++ gps_accuracy/main.m
@@ -1,39 +1,31 @@
 format long g
 clear all
 
-%csv = csvread("../dataset/fengyuan_20210705.csv");
-csv = csvread("./gps_accuracy_log.csv");
+csv = csvread("gps_acc.csv");
 
-%ms
-%timestamp_ms = csv(:, 1);
-%m/s^2
-h_acc = csv(:, 1);
-v_acc = csv(:, 2);
-
-[data_num, dummy] = size(h_acc);
-timestamp_s = zeros(1, data_num);
-dt = 1 / 100;
-for i = 2: data_num
-    timestamp_s(i) = (i - 1) .* dt;
-end
+time_ms = csv(:, 1);
+time_ms = time_ms - time_ms(1);
+times_s = time_ms .* 1e-3;
+h_acc = csv(:, 2);
+v_acc = csv(:, 3);
 
 %%%%%%%%
 % Plot %
 %%%%%%%%
 
 %accelerometer
-figure('Name', 'accelerometer');
+figure('Name', 'GNSS Accuracy');
 subplot (2, 1, 1);
-plot(timestamp_s, h_acc);
-title('accelerometer');
+plot(times_s, h_acc);
+title('GNSS accuracy');
 xlabel('time [s]');
-ylabel('ax [m/s^2]');
-xlim([0 timestamp_s(end)]) 
+ylabel('Horizontal accuracy [m]');
+xlim([0 times_s(end)]) 
 subplot (2, 1, 2);
-plot(timestamp_s, v_acc);
+plot(times_s, v_acc);
 xlabel('time [s]');
-ylabel('ay [m/s^2]');
-xlim([0 timestamp_s(end)]) 
+ylabel('Vertical accuracy [m]');
+xlim([0 times_s(end)]) 
 
 disp("Press any key to leave");
 pause;
